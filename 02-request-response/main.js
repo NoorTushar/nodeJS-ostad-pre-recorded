0cc5165221b6d1f48d7a5c5b9717d20c60e99076
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-02 05:12:59 +0600

    request

---
+++ 02-request-response/main.js
@@ -0,0 +1,23 @@
+const http = require(`http`);
+
+const server = http.createServer((req, res) => {
+   if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<h1>This is Home Page</h1>`);
+      res.end();
+   } else if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<h1>This is About Page</h1>`);
+      res.end();
+   } else if (req.url === "/contact") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<h1>This is Contact Page</h1>`);
+      res.end();
+   }
+});
+
+const port = 5050;
+
+server.listen(port, () => {
+   console.log(`server is running`);
+});
