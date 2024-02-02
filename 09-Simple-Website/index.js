f6a3d7bf5b404a8e8c09ee705af02b64efff2b85
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-02 21:13:02 +0600

    first simple website created by NodeJS

---
+++ 09-Simple-Website/index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+   if (req.url === "/" || req.url === "/home") {
+      let data = fs.readFileSync("home.html", "utf-8");
+      res.end(data);
+   } else if (req.url === "/about") {
+      let data = fs.readFileSync("about.html", "utf-8");
+      res.end(data);
+   } else if (req.url === "/contact") {
+      let data = fs.readFileSync("contact.html", "utf-8");
+      res.end(data);
+   } else if (req.url === "/terms") {
+      let data = fs.readFileSync("terms.html", "utf-8");
+      res.end(data);
+   }
+});
+
+const port = 5500;
+server.listen(port);
+
+console.log(`server running successfully at port: ${port}`);
