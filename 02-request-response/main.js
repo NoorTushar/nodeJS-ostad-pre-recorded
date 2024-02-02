d01aab3fe9d2678b32ecce5b7deeb66690088c54
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-02 14:14:41 +0600

    req res

--- 02-request-response/main.js
+++ 02-request-response/main.js
@@ -1,9 +1,10 @@
+/*
 const http = require(`http`);
 
 const server = http.createServer((req, res) => {
    if (req.url === "/") {
       res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(`<h1>This is Home Page</h1>`);
+      res.write(`<h1>This is Home Page....</h1>`);
       res.end();
    } else if (req.url === "/about") {
       res.writeHead(200, { "Content-Type": "text/html" });
@@ -20,4 +21,31 @@ const port = 5050;
 
 server.listen(port, () => {
    console.log(`server is running`);
+});
+
+
+*/
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+   if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("This is Home Page");
+      res.end();
+   } else if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("This is About Page");
+      res.end();
+   } else if (req.url === "/contact") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("This is Contact Page");
+      res.end();
+   }
+});
+
+const port = 5500;
+
+server.listen(port, () => {
+   console.log(`server is running at port ${port}`);
 });
