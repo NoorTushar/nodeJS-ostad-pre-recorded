347b987e40a922eda66d7512103cb54d63f71b5b
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-02 17:54:29 +0600

    synchronous file read

--- 04-fs-module/main.js
+++ 04-fs-module/main.js
@@ -1,8 +1,17 @@
 const http = require("http");
+
+// new
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-   res.end("bye");
+   if (req.url === "/") {
+      // new
+      fs.readFile("home.html", (error, data) => {
+         res.writeHead(200, { "Content-Type": "text/html" });
+         res.write(data);
+         res.end();
+      });
+   }
 });
 
 const port = 5500;
@@ -10,3 +19,29 @@ const port = 5500;
 server.listen(port, () => {
    console.log(`server is running successfully`);
 });
+
+/*
+// Synchronous Way
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+   // asynchronous method:
+   if (req.url === "/") {
+      const data = fs.readFileSync("home.html");
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+   }
+});
+
+const port = 5500;
+
+server.listen(port, () => {
+   console.log(`server is running successfully`);
+});
+
+
+*/
