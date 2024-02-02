4322270e23334dda3987f220f411ea3a723e0e56
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-02 18:55:20 +0600

    learning fs rename

--- 05-fs-writeFile/main.js
+++ 05-fs-writeFile/main.js
@@ -3,22 +3,44 @@ const fs = require("fs");
 
 const port = 5500;
 
+// async
+
+// const server = http.createServer((req, res) => {
+//    if (req.url === "/") {
+//       fs.writeFile("demo.txt", "this is created by NodeJS", (error) => {
+//          if (error) {
+//             res.writeHead(200, { "Content-Type": "text/html" });
+//             res.write("ERROR in writing file");
+//             res.end();
+//          } else {
+//             res.writeHead(200, { "Content-Type": "text/html" });
+//             res.write("Success in Writing File");
+//             res.end();
+//          }
+//       });
+//    }
+// });
+
+// sync
+
 const server = http.createServer((req, res) => {
    if (req.url === "/") {
-      fs.writeFile("demo.txt", "this is created by NodeJS", (error) => {
-         if (error) {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write("ERROR in writing file");
-            res.end();
-         } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write("Success in Writing File");
-            res.end();
-         }
-      });
+      let error = fs.writeFileSync(
+         "synchronous.txt",
+         "this is created by NodeJS synchronous way."
+      );
+
+      if (error) {
+         res.writeHead(200, { "Content-Type": "text/html" });
+         res.write("ERROR in writing file");
+         res.end();
+      } else {
+         res.writeHead(200, { "Content-Type": "text/html" });
+         res.write("Success in Writing File");
+         res.end();
+      }
    }
 });
-
 server.listen(port, () => {
    console.log(`server is running`);
 });
